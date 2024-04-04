5236b52eb13a8d79898c541688c36d0c4a9b281f
KGurov <kosta989837@gmail.com>
2024-04-04 20:41:16 +0300

    end

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -1,110 +1,84 @@
 package com.tictactoe;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "LogicServlet", value = "/logic")
+@WebServlet("/logic")
 public class LogicServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Получаем текущую сессию
-        HttpSession currentSession = req.getSession();
 
-        // Получаем объект игрового поля из сессии
+        HttpSession currentSession = req.getSession();
         Field field = extractField(currentSession);
 
-        // получаем индекс ячейки, по которой произошел клик
-        int index = getSelectedIndex(req);
+        int index = getSelectIndex(req);
         Sign currentSign = field.getField().get(index);
 
-        // Проверяем, что ячейка, по которой был клик пустая.
-        // Иначе ничего не делаем и отправляем пользователя на ту же страницу без изменений
-        // параметров в сессии
-        if (Sign.EMPTY != currentSign) {
-            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
-            dispatcher.forward(req, resp);
+        if (currentSign != Sign.EMPTY) {
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/index.jsp");
+            requestDispatcher.forward(req, resp);
             return;
         }
 
-        // ставим крестик в ячейке, по которой кликнул пользователь
         field.getField().put(index, Sign.CROSS);
-
-        if (checkWin(resp, currentSession, field)) {
+        if (checkWin(currentSession, field, resp)) {
             return;
         }
 
-        // Получаем пустую ячейку поля
         int emptyFieldIndex = field.getEmptyFieldIndex();
-
         if (emptyFieldIndex >= 0) {
             field.getField().put(emptyFieldIndex, Sign.NOUGHT);
-            // Проверяем, не победил ли нолик после добавление последнего нолика
-            if (checkWin(resp, currentSession, field)) {
+            if (checkWin(currentSession, field, resp)) {
                 return;
             }
-        }
-        else {
-            // Добавляем в сессию флаг, который сигнализирует что произошла ничья
+        } else {
             currentSession.setAttribute("draw", true);
-
-            // Считаем список значков
-            List<Sign> data = field.getFieldData();
-
-            // Обновляем этот список в сессии
-            currentSession.setAttribute("data", data);
-
-            // Шлем редирект
-            resp.sendRedirect("/index.jsp");
-            return;
         }
 
-        // Считаем список значков
         List<Sign> data = field.getFieldData();
 
-        // Обновляем объект поля и список значков в сессии
         currentSession.setAttribute("data", data);
         currentSession.setAttribute("field", field);
 
         resp.sendRedirect("/index.jsp");
     }
 
-    private boolean checkWin(HttpServletResponse response, HttpSession currentSession, Field field) throws IOException {
+    private boolean checkWin(HttpSession session, Field field, HttpServletResponse resp) throws IOException {
         Sign winner = field.checkWin();
+
         if (Sign.CROSS == winner || Sign.NOUGHT == winner) {
-            // Добавляем флаг, который показывает что кто-то победил
-            currentSession.setAttribute("winner", winner);
-
-            // Считаем список значков
+            session.setAttribute("winner", winner);
             List<Sign> data = field.getFieldData();
-
-            // Обновляем этот список в сессии
-            currentSession.setAttribute("data", data);
-
-            // Шлем редирект
-            response.sendRedirect("/index.jsp");
+            session.setAttribute("data", data);
+            resp.sendRedirect("/index.jsp");
             return true;
         }
+
         return false;
     }
 
-    private int getSelectedIndex(HttpServletRequest request) {
-        String click = request.getParameter("click");
-        boolean isNumeric = click.chars().allMatch(Character::isDigit);
-        return isNumeric ? Integer.parseInt(click) : 0;
-    }
     private Field extractField(HttpSession currentSession) {
         Object fieldAttribute = currentSession.getAttribute("field");
+
         if (Field.class != fieldAttribute.getClass()) {
             currentSession.invalidate();
-            throw new RuntimeException("Session is broken, try one more time");
+            throw new RuntimeException("Session is broken, please try again.");
         }
         return (Field) fieldAttribute;
+    }
+
+    private int getSelectIndex(HttpServletRequest req) {
+        String click = req.getParameter("click");
+        boolean isDigit = click.chars().allMatch(Character::isDigit);
+        return isDigit ? Integer.parseInt(click) : 0;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,11 +1,11 @@
 <%@ page import="com.tictactoe.Sign" %>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!DOCTYPE html>
 <html>
 <head>
     <link href="static/main.css" rel="stylesheet">
-    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
     <script src="<c:url value="/static/jquery-3.6.0.min.js"/>"></script>
     <title>Tic-Tac-Toe</title>
 </head>
@@ -13,35 +13,33 @@
 <h1>Tic-Tac-Toe</h1>
 <table>
     <tr>
-        <td onclick="window.location='/logic?click=0'">${data.get(0).getSign()}</td>>
-        <td onclick="window.location='/logic?click=1'">${data.get(1).getSign()}</td>>
-        <td onclick="window.location='/logic?click=2'">${data.get(2).getSign()}</td>>
+        <td onclick="window.location='/logic?click=0'">${sessionScope.data.get(0).getSign()}</td>
+        <td onclick="window.location='/logic?click=1'">${sessionScope.data.get(1).getSign()}</td>
+        <td onclick="window.location='/logic?click=2'">${sessionScope.data.get(2).getSign()}</td>
     </tr>
     <tr>
-        <td onclick="window.location='/logic?click=3'">${data.get(3).getSign()}</td>>
-        <td onclick="window.location='/logic?click=4'">${data.get(4).getSign()}</td>>
-        <td onclick="window.location='/logic?click=5'">${data.get(5).getSign()}</td>>
+        <td onclick="window.location='/logic?click=3'">${sessionScope.data.get(3).getSign()}</td>
+        <td onclick="window.location='/logic?click=4'">${sessionScope.data.get(4).getSign()}</td>
+        <td onclick="window.location='/logic?click=5'">${sessionScope.data.get(5).getSign()}</td>
     </tr>
     <tr>
-        <td onclick="window.location='/logic?click=6'">${data.get(6).getSign()}</td>>
-        <td onclick="window.location='/logic?click=7'">${data.get(7).getSign()}</td>>
-        <td onclick="window.location='/logic?click=8'">${data.get(8).getSign()}</td>>
+        <td onclick="window.location='/logic?click=6'">${sessionScope.data.get(6).getSign()}</td>
+        <td onclick="window.location='/logic?click=7'">${sessionScope.data.get(7).getSign()}</td>
+        <td onclick="window.location='/logic?click=8'">${sessionScope.data.get(8).getSign()}</td>
     </tr>
 </table>
-<hr>
-<c:set var="CROSSES" value="<%=Sign.CROSS%>"/>
-<c:set var="NOUGHTS" value="<%=Sign.NOUGHT%>"/>
-
-<c:if test="${winner == CROSSES}">
+<hr/>
+<c:if test="${sessionScope.draw}">
+    <h1>IT'S A DRAW!</h1>
+    <br>
+    <button onclick="restart()">Start again</button>
+</c:if>
+<c:if test="${Sign.CROSS == sessionScope.winner}">
     <h1>CROSSES WIN!</h1>
     <button onclick="restart()">Start again</button>
 </c:if>
-<c:if test="${winner == NOUGHTS}">
+<c:if test="${Sign.NOUGHT == sessionScope.winner}">
     <h1>NOUGHTS WIN!</h1>
-    <button onclick="restart()">Start again</button>
-</c:if>
-<c:if test="${draw}">
-    <h1>IT'S A DRAW</h1>
     <button onclick="restart()">Start again</button>
 </c:if>
 <script>
